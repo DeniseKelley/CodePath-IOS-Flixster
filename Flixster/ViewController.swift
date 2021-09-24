e41d3689016b7ad6b2885ff1bdef8712cdc0a1db
DeniseKelley <66845312+DeniseKelley@users.noreply.github.com>
2021-09-24 14:40:11 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Anna Kelley on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
