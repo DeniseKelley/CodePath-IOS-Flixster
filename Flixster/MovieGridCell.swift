1eaf1b78a9b4bb105ab89d5cd278832c65fd99a2
DeniseKelley <66845312+DeniseKelley@users.noreply.github.com>
2021-10-01 17:19:12 -0400

    committing files!

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Anna Kelley on 10/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
